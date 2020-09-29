737146c7f68a30ea43d73fb28f6eb61ab5cdcb40
IsmailMarghich <116674@leerling.calandlyceum.nl>
2020-09-29 21:41:01 +0200

    update

--- Python/Python For Everybody/Chapter7.2_files.py
+++ Python/Python For Everybody/Chapter7.2_files.py
@@ -1,16 +1,16 @@
-fname = input("Enter file name: ")
-try:
+fname = input("Enter file name: ") #input filename
+try: #try opening file
     fh = open(fname)
-except:
+except: #if file isnt recognized, start over again
     print('File cannot be opened')
     exit()
-count = 0
+count = 0 #declaring starting variables
 total = 0.0
 for line in fh:
-    if line.startswith('X-DSPAM-Confidence: '):
-        x = line.find(":")
-        y = float(line[x+1:])
-        count += 1
-        total += y
-print('Average spam confidence:', total/count)
-print(count)
+    if line.startswith('X-DSPAM-Confidence: '): #finding lines that start with our target
+        x = line.find(":") #find location of :
+        y = float(line[x+1:]) #filter out the float from the target
+        count += 1 #add to the count
+        total += y #add to the total
+print('Average spam confidence:', total/count) #print the average confidence
+print(count) #print count
--- Python/Python For Everybody/chapter6_strings.py
+++ Python/Python For Everybody/chapter6_strings.py
@@ -1,3 +1,3 @@
-text = "X-DSPAM-Confidence:    0.8475";
-x = text.find(':')
-print(float((text[x+1:])))
+text = "X-DSPAM-Confidence:    0.8475"; #find and slice the string so we only have the numbers
+x = text.find(':') #find the location of the :
+print(float((text[x+1:]))) #print the float numbers after but not including the :

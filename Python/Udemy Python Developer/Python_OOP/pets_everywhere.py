b91effbef6ec1bb40af4154ce7e90a2560bc14d5
IsmailMarghich <116674@leerling.calandlyceum.nl>
2020-11-03 23:56:38 +0100

    renaming

---
+++ Python/Udemy Python Developer/Python_OOP/pets_everywhere.py
@@ -0,0 +1,8 @@
+class PlayerCharacter:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def speak(self):
+        return (f'my name is {self.name}, and I am {self.age}, years old')
+player1 = PlayerCharacter('andrei', 100)
+print(player1.speak()) 

b98d43e08a70e44c46d815991ee713d008a11b74
IsmailMarghich <116674@leerling.calandlyceum.nl>
2020-11-13 21:18:18 +0100

    Done

--- Python/Udemy Python Developer/Python_Generators/fibonacci_numbers.py
+++ Python/Udemy Python Developer/Python_Generators/fibonacci_numbers.py
@@ -1,5 +1,11 @@
-
-def make_list(num, i):
-    x = num * 2**i
-    return x
-print(make_list(5,3))
+def fib(number): #create a function that generates fibonacci numbers using a generator.
+    a = 0  #starting variables
+    b = 1
+    for i in range(number): #iterate how many times with the input
+        yield a #hold the original a
+        temp = a #create a temp for the new a
+        a = b #number moves up
+        b = temp + b #add the temp a to the second number b
+    return b #return the second number when the iteration is done
+for x in fib(10): 
+    print(x)

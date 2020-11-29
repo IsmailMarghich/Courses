ab4d62208b3948c72ecdc0f5ebc88b8ba0d794b5
IsmailMarghich <116674@leerling.calandlyceum.nl>
2020-11-29 17:50:54 +0100

    basic website

--- Python/Udemy Python Developer/Projects/Python_Web_Development/venv/app.py
+++ Python/Udemy Python Developer/Projects/Python_Web_Development/venv/app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 @app.route('/')
-def hello_world():
-    return 'Runescape is the best game'
+def route():
+    return redirect('http://127.0.0.1:5000/index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'form submitted yay'
 
 if __name__ == "__main__":
     app.run(debug=True)

7c95b894bc411a10560a5b5a098900fd97b7cd0d
IsmailMarghich <116674@leerling.calandlyceum.nl>
2020-11-30 17:57:35 +0100

    update

--- Python/Udemy Python Developer/Projects/Python_Web_Development/venv/app.py
+++ Python/Udemy Python Developer/Projects/Python_Web_Development/venv/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
@@ -11,7 +11,12 @@ def html_page(page_name):
     return render_template(page_name)
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return 'form submitted yay'
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong'
 
 if __name__ == "__main__":
     app.run(debug=True)

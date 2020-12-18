81d775e67634ee80705dd48d624ce2ebf298aaf6
IsmailMarghich <116674@leerling.calandlyceum.nl>
2020-12-18 22:24:15 +0100

    done

--- Python/Python_DS_Algorithms/Linked_lists/Linked_List_practice.py
+++ Python/Python_DS_Algorithms/Linked_lists/Linked_List_practice.py
@@ -1,17 +1,17 @@
 from LinkedListClass import LinkedList
 #1 Remove duplicates from an unsorted linked list
 def removeduplicates(ll):
-    if ll.head is None:
+    if ll.head is None: #check if list exists
         return
     else:
-        currentNode = ll.head
-        visited = set([currentNode.value])
+        currentNode = ll.head #attach to first node
+        visited = set([currentNode.value]) #make a set where we add our duplicated values
         while currentNode.next:
-            if currentNode.next.value in visited:
-                currentNode.next = currentNode.next.next
+            if currentNode.next.value in visited: #if node already in duplicate set
+                currentNode.next = currentNode.next.next #continue
             else:
-                visited.add(currentNode.next.value)
-                currentNode = currentNode.next
+                visited.add(currentNode.next.value) #otherwise add it to duplicates set
+                currentNode = currentNode.next #continue
         return ll
 customLL = LinkedList()
 customLL.randomlist(10, 0, 10)

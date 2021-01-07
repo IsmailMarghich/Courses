6e247f36f60020bd9b1a3998723920df5314b0ec
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-01-07 23:08:46 +0100

    done

---
+++ Beginner_C/structs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//a struct can store multiple variables
+int main ( )
+{
+    struct date //defining struct date
+    {
+        int year;
+        int month; //definding variables in the struct
+        int day;
+    } today; //here we can also add the name after the struct, to initialize the struct in today
+
+    today.year = 2021;//we can assign variables to structs by using . to access members
+    today.month = 1;
+    today.day = 7;
+
+    printf("%d %d %d\n", today.year, today.month, today.day);
+    struct date tommorow = {.year = 2021, .month = 1, .day = 8}; //we can also declare the struct in one go
+
+    printf("%d %d %d", tommorow.year, tommorow.month, tommorow.day);
+
+
+}

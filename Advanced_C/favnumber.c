475be753797eb481cf4476a1b9f97ef759a2b09b
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-01-15 01:03:09 +0100

    done

---
+++ Advanced_C/favnumber.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "favnumber.h"
+//this file is to demonstrate how we can divide the program in header files
+//the getfavoritenumber function is defined in a header file, but we can still use it here by including it
+int main() {
+    printf("%d\n", getfavoritenumber());
+    return 0;
+}

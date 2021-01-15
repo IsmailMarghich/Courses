70caf2c39f5a83b31e60970ef074a555ecdae3cd
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-01-16 00:20:48 +0100

    done

--- Advanced_C/externalvariables.c
+++ Advanced_C/externalvariables.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int globalnumber = 6;
+static int globalnumber = 6; //static means each file will make a copy of this variable instead of using the variable itself
 void globalfunction(void)
 {
     printf("you just used a global function\n");
---
+++ Advanced_C/typequalifiers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//type qualifiers can be used in front of variables to give the compiler about the intended use of the variables
+int main ( )
+{
+    //const
+    //a const variable cannot be changed, this means the compiler can put this variable in readonly memory, which leaves more ram memory for other variables
+
+    const int x = 5; //constant variable
+    const int * const pointer; //with pointers, if u use constant before the star, it means the file its point to is constant
+                               //if const is after the star, it means the address that the pointer points to is constant
+    //volatile
+    //volatile is the opposite of const, where volatile tells the compiler that this variable will change
+    //normally, sometimes variables are cached in registers so accessing this variable would be faster
+    //sometimes this is not desirable because of threads needing to access the original location of the variable
+    volatile int y = 5; //this variable will not be cached in register, and threads will thus always access the most up to date version of the variable
+
+    //restrict
+    //the restrict type qualifier is an optimization hint for the compiler, it means that a certain pointer is the only reference to a certain value
+    //because a restrict pointer is the only one pointing to this data, it means the compiler does not to add any additional checks, which improves performance
+    int number = 5;
+    int * restrict intpointer = NULL; //because this pointer is the only one pointing to this number, we can use restrict so the compiler can optimize the code
+    intpointer = &number;
+    printf("%d", *intpointer);
+
+
+
+    return 0;
+}

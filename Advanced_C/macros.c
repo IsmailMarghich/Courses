ab8979a6a25d039d96a489de00abb25e27ec4256
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-01-20 20:07:19 +0100

    done

--- Advanced_C/macros.c
+++ Advanced_C/macros.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 //in this file we will learn about macros and make our own
-//macros are similar to functions, but they are defined in the preprocessor and can make a programmers time easier, we have used macros before
-#define PLUS(a,b) \
-printf("%d", a+b);
+//macros are similar to functions, but they are preprocessed, when a macro is called, the macro body is inserted directly into the code, we have used macros before
+#define PRINTPLUS(a, b) \
+printf("%d\n", a+b);
+
+#define UAREFUNNY(name) \
+printf(#name" Is funny\n") //we can use # to insert a token, i,e a variable like in this example
+
 //normally macros are only one a single line but with \ we can use multiple lines
-int main ( )
-{
+//a macro is faster than a function since it does not need to be compiled and the macro itself can be immediately replaced by its body
+int main() {
     int a = 5;
     int b = 5;
-    PLUS(a,b);//making use of our PLUS macro to print the sum of 2 ints
+    PRINTPLUS(a, b);//making use of our PLUS macro to print the sum of 2 ints
+    //its harder to debug a macro because its preprocessed before the program is compiled, it does not have typed arguments.
+    //inline functions can be used instead, they can be debugged and have type checking but inline keyword is only a suggestion to the compiler
+
+    UAREFUNNY(Ismail);
+    //there are default macros that C offers for us
+    printf("%s\n", __FILE__); //returns file name
+    printf("%d\n", __LINE__); //returns number of line
+    printf("%s\n", __FUNCTION__); //returns name of function
+    printf("%s\n", __TIME__); //returns time
+    printf("%s\n", __DATE__); //returns date
+    printf("%d\n", __STDC__); //returns 1 if current compiler conforms to ISO standard C compiler, 0 if not
+
+    return 0;
 }
--- Advanced_C/unions.c
+++ Advanced_C/unions.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int main ( )
-{
+//this file is all about unions, unions are similar to structs where they can store data together, but it can store multiple different types of data
+//it also does not use memory for unused variables, unlike structs
+//unions store the different types of variable in the same memory spot unlike structs which does that separately, thus saving you memory
+//the issue with unions however is that only one member of a union can be accessed at a time, and only the first member can be initialized
+//thus unions should used when saving memory is important and when u have specialized data
+int main() {
+    union car {
+        int i;
+        float j; //a union will allocate memory the same as the largest element in union
+        int k;
+    };
+    union car car1, car2, *carpointer = &car2; //we declare 2 normal unions and 1 pointer union pointing to the second union
+    printf("Memory size occupied by data:%zu\n", sizeof(car1)); //float are 4 bytes, so it prints 4
+    car1.j = 10.0; //assigning values to our unions works just like structs
+    printf("%f\n", car1.j);
+    printf("%d\n",
+           car1.i);//this will return random numbers(memory address) because only 1 value can be accessed and initialized at a time
+    car1.i = 5;
+    printf("%d\n", car1.i);//now that car1.i is initialized, it can be printed
+    printf("%f\n",
+           car1.j); //however this will again return the wrong number, because car1.i has already been initialized, there is only 1 spot
 
+    carpointer->i = 6; //here we can use our pointer to set the i variable in union car2
+    printf("%d", carpointer->i); //we use -> for pointers just like with structs
     return 0;
 }

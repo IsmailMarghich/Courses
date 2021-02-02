3f8ca755c599e1bcc48c869dfc8dd92642031165
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-02-02 16:16:42 +0100

    moved to different repo

--- Python/Udemy Python Developer/Exercises/lamba_expressions.py
+++
@@ -1,7 +0,0 @@
-#create a lambda expression that will square a list
-my_list = [1,2,3]
-print(list(map(lambda x: x**2, my_list))) #each item in the list is x, and it gets squared
-#sort a list based on the lowest value in a tuple
-a = [(0,2), (4,3), (9,9), (10,-1)]
-a.sort(key=lambda x: x[1]) #we want the second value in the tumple, so we define x as the second,(1) index of our tuple
-print(a)
--- Python/Udemy Python Developer/Exercises/walrusoperator.py
+++
@@ -1,7 +0,0 @@
-a = 'helloooooooo'
-if ((n := len(a)) > 10):
-   print('too long', n, 'elements')
-
-while (n := len(a)) > 1:
-    print(n)
-    a = a[:-1]

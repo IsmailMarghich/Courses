82764212650d02a63bce80a26046d6bf392b7a95
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-02-23 00:49:19 +0100

    done

---
+++ C++/cmake-build-debug/CMakeFiles/Beginner__.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Gebruiker/Desktop/Coding/Courses/C++/Beginner_C++/files.cpp" "C:/Users/Gebruiker/Desktop/Coding/Courses/C++/Beginner_C++/cmake-build-debug/CMakeFiles/Beginner__.dir/files.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C++/cmake-build-debug/CMakeFiles/Beginner__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Beginner__.exe"
+  "Beginner__.exe.manifest"
+  "Beginner__.pdb"
+  "CMakeFiles/Beginner__.dir/files.cpp.obj"
+  "libBeginner__.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Beginner__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

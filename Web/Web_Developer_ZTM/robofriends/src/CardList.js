21915d58b48c6397f46ff7e2d370fc3d977149c9
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-03-09 01:20:47 +0100

    done

---
+++ Web/Web_Developer_ZTM/robofriends/src/CardList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from "./Card";
+import {robots} from "./robots";
+
+const CardList = ({robots}) =>{
+    const cardComponents = robots.map((user, index) =>{ /*functions that maps returning a card to our list of of objects*/
+        return <Card
+            key={index}
+            id={robots[index].name}
+            name={robots[index].name}
+            email={robots[index].email}/>
+    })
+    return(
+        <div>
+            {cardComponents} {/*call the function, returns all the cards from robots.js*/}
+        </div>
+    )
+}
+export default CardList;

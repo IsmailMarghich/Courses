3b3106f31a3b931ced6b5998ed6876f20e752a00
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-03-12 21:33:09 +0100

    clarifai api died

--- Web/Web_Developer_ZTM/facedetector/src/App.js
+++ Web/Web_Developer_ZTM/facedetector/src/App.js
@@ -5,24 +5,95 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
-const particleOptions = {"particles": {
-    "number": {
-        "value": 50
+import Clarifai from 'clarifai'
+const app = new Clarifai.App({
+    apiKey: 'b02f8720fe20454190112fa90dba9255'
+});
+const particleOptions = {
+    "particles": {
+        "number": {
+            "value": 50
+        },
+        "size": {
+            "value": 3
+        }
     },
-    "size": {
-        "value": 4
-    }
-}, /*our options for our particles background*/
-"interactivity": {
-    "events": {
-        "onhover": {
-            "enable": true,
-            "mode": "grab"
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": true,
+                    "mode": "repulse"
+            }
         }
     }
 }
-}
-class App extends Component{
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {},
+            route: 'signin',
+            isSignedIn: false,
+            user: {
+                id: '',
+                name: '',
+                email: '',
+                entries: 0,
+                joined: ''
+            }
+        }
+    }
+
+    loadUser = (data) => {
+        this.setState({user: {
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                entries: data.entries,
+                joined: data.joined
+            }})
+    }
+    onInputChange = (event) =>{
+    console.log(event.target.value);
+    }
+    onSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        app.models
+            .predict(
+                // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+                // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+                // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+                // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+                // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+                // so you would change from:
+                // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+                // to:
+                // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+                'c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+            .then(response => {
+                console.log('hi', response)
+                if (response) {
+                    fetch('https://pbs.twimg.com/profile_images/1116799956013203462/WOX874ki.png', {
+                        method: 'put',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({
+                            id: this.state.user.id
+                        })
+                    })
+                        .then(response => response.json())
+                        .then(count => {
+                            console.log(response);
+                            this.setState(Object.assign(this.state.user, { entries: count}))
+                        })
+
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
         <div className="App">
@@ -32,7 +103,8 @@ class App extends Component{
             <Navigation/>
             <Logo/>
             <Rank />
-            <ImageLinkForm/>
+            <ImageLinkForm onInputChange={this.onInputChange}
+                           onSubmit = {this.onSubmit}/>
 {/*
 
             <FaceRecognition/>*/}
--- Web/Web_Developer_ZTM/facedetector/src/components/ImageLinkForm/ImageLinkForm.js
+++ Web/Web_Developer_ZTM/facedetector/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ImageLinkForm = () =>{ /*a logout button*/
+const ImageLinkForm = ({onInputChange, onSubmit}) =>{ /*a logout button*/
     return(
         <div>
             <p
@@ -8,8 +8,11 @@ const ImageLinkForm = () =>{ /*a logout button*/
                 {'This magic brain will detect your face!'}</p>
             <div className='pa5 br3 shadow-5'>
                 <div>
-                <input className='f4 pa2 w-70 center' type='text' />
-                <button style= {{alignContent: 'center'}} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple center'>Detect</button>
+                <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+                <button style= {{alignContent: 'center'}}
+                        className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple center'
+                    onClick = {onSubmit}
+                    >Detect</button>
             </div>
             </div>
         </div>

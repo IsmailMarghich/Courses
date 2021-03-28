30f2a669d19fa5cde5e35c196921abae7ad93a51
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-03-28 20:51:08 +0200

    done

--- Web/20_Js_Projects/video_player/script.js
+++ Web/20_Js_Projects/video_player/script.js
@@ -51,12 +51,11 @@ const setProgress = (e) => {
     const newTime = e.offsetX / progressRange.offsetWidth; /*this returns a ratio which we can use to change progress*/
     progressBar.style.width = `${newTime * 100}%`; /*change progressbar visually*/
     video.currentTime = newTime * video.duration /*change progress of video by calculating time*/
-    console.log(newTime)
 }
 // Volume Controls --------------------------- //
 
 /*Volume bar*/
-let lastVolume = 1
+let lastVolume = 1 /*var for whether sound on/off*/
 
 const changeVolume = (e) => {
     let volume = e.offsetX / volumeRange.offsetWidth /*volume is ratio of the bar u press on*/
@@ -81,17 +80,60 @@ const changeVolume = (e) => {
     }else if(volume === 0){
         volumeIcon.classList.add('fas','fa-volume-off')
     }
+    lastVolume = volume; /*make sure to update volume boolean after changing volume*/
+}
+/*Mute/unmute*/
 
+const toggleMute = () => {
+    volumeIcon.className = ''; /*reset classes*/
+    if (video.volume) { /*trigger if volume is anything but zero*/
+        lastVolume = video.volume
+        video.volume = 0; /*mute*/
+        volumeBar.style.width = 0;
+        volumeIcon.classList.add('fas','fa-volume-mute')
+        volumeIcon.setAttribute('title', 'Unmute')
+    } else {
+        video.volume = lastVolume; /*unmute*/
+        volumeBar.style.width = `${lastVolume * 100}%` /*get % of volumebar*/
+        volumeIcon.classList.add('fas','fa-volume-up')
+        volumeIcon.setAttribute('title', 'Mute')
+
+    }
 }
 
-
-// Change Playback Speed -------------------- //
-
-
-
 // Fullscreen ------------------------------- //
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    video.classList.add('video-fullscreen') /*make video fill up the whole screen*/
+}
 
-
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    video.classList.remove('video-fullscreen') /*make video small again*/
+}
+let fullscreen = false; /*boolean for fullscreen or not*/
+/*Toggle fullscreen*/
+const toggleFullscreen = () => {
+    if (!fullscreen){
+        openFullscreen(player)
+    }else {
+        closeFullscreen();
+    }
+    fullscreen = !fullscreen /*reverse value of fullscreen boolean*/
+}
 /*Event listeners*/
 playButton.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay); /*make it so we can pause and play whenever we click on the video*/
@@ -99,3 +141,8 @@ video.addEventListener('timeupdate', updateProgress); /*everytime time updates, 
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress)
 volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', () => {
+    video.playbackRate = speed.value; /*set video playback rate to value of select*/
+});
+fullscreenBtn.addEventListener('click', toggleFullscreen);

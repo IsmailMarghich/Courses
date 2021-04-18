6c1ec64e49a81660861e35c83605840484759b08
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-04-18 16:34:26 +0200

    project

--- Angular/cards/src/app/app.component.ts
+++ Angular/cards/src/app/app.component.ts
@@ -9,20 +9,23 @@ export class AppComponent {
   posts = [
     {
       title: 'Neat Tree',
-      imageUrl: 'assets/tree.jpg',
-      username: 'Nature',
+      imageUrl:
+        'https://www.gardeningknowhow.com/wp-content/uploads/2010/09/tree-with-hole.png',
+      username: '@Nature',
       content: 'I saw this neat tree while on my walk.',
     },
     {
       title: 'Snowy mountain',
-      imageUrl: 'assets/mountain.jpg',
-      username: 'Mountain_Climber',
+      imageUrl:
+        'https://desenio.nl/bilder/artiklar/zoom/11634_2.jpg?imgwidth=435&qt=',
+      username: '@Mountain_Climber',
       content: 'This mountain is huge!',
     },
     {
       title: 'Mountain Biking',
-      imageUrl: 'assets/biking.jpg',
-      username: 'biker1337',
+      imageUrl:
+        'https://s14761.pcdn.co/wp-content/uploads/2020/09/Propain-spindrift-cf-2021-enduro-test-review36-810x551.jpg',
+      username: '@biker1337',
       content: 'Went on a lovely bike ride today.',
     },
   ];

8fb664c59976bb738c4aff71177b24c5335ad5d7
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-04-18 03:12:06 +0200

    progress

--- Angular/cards/src/app/app.component.ts
+++ Angular/cards/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpg',
+      username: 'Nature',
+      content: 'I saw this neat tree while on my walk.',
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/mountain.jpg',
+      username: 'Mountain_Climber',
+      content: 'This mountain is huge!',
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpg',
+      username: 'biker1337',
+      content: 'Went on a lovely bike ride today.',
+    },
+  ];
 }

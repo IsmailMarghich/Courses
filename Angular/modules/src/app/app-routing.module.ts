1a489fe8c509477c26db5aabbb7bef522356984e
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-04-21 20:51:26 +0200

    progress

---
+++ Angular/modules/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  /*when a user goes to /elements on the website, it goes to our component*/
+  {
+    path: 'elements',
+    loadChildren: () =>
+      /*we can implement lazy loading by only importing the elements component whenever the user goes there, this improves performance*/
+      import('./elements/elements.module').then((m) => m.ElementsModule),
+  },
+  {
+    path: 'collections',
+    loadChildren: () =>
+      import('./collections/collections.module').then(
+        (m) => m.CollectionsModule
+      ),
+  },
+  { path: '', component: HomeComponent },
+  /* double * path is any path that we dont already have*/
+  { path: '**', component: NotFoundComponent },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}

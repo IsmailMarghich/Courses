e80747722318b5490a517f7d15f38737458ee6c8
IsmailMarghich <116674@leerling.calandlyceum.nl>
2021-04-22 01:52:32 +0200

    progress #2

--- Angular/modules/src/app/collections/collections-home/collections-home.component.ts
+++ Angular/modules/src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'Designer' },
+    { name: 'John', age: 32, job: 'Senior Developer' },
+    { name: 'Jay', age: 19, job: 'Junior Developer' },
+  ];
+  /*we use headers so we dont mess up our table when we add more values to data array*/
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
